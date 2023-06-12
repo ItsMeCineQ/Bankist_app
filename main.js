0e0f557e140196071c4a178532be011ee086275d
ItsMeCineQ <131790357+ItsMeCineQ@users.noreply.github.com>
2023-06-12 21:20:15 +0200

    updated html and css and added query selectors to code

--- main.js
+++ main.js
@@ -20,3 +20,37 @@ const account3 = {
     interestRate: 0.7, // %
     pin: 3333,
 };
+
+const account4 = {
+    owner: 'Sarah Smith',
+    movements: [430, 1000, 700, 50, 90],
+    interestRate: 1,
+    pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.current-balance');
+const labelSumIn = document.querySelector('.summary_value-in');
+const labelSumOut = document.querySelector('.summary_value-out');
+const labelSumInterest = document.querySelector('.summary_value-interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.left');
+
+const btnLogin = document.querySelector('.login');
+const btnTransfer = document.querySelector('.transfer');
+const btnLoan = document.querySelector('.btnLoan');
+const btnClose = document.querySelector('.close');
+const btnSort = document.querySelector('.sort');
+
+const inputLoginUsername = document.querySelector('.username');
+const inputLoginPin = document.querySelector('.pin');
+const inputTransferTo = document.querySelector('.form_input-to');
+const inputTransferAmount = document.querySelector('.form_input-amount');
+const inputLoanAmount = document.querySelector('.form_input-loan');
+const inputCloseUsername = document.querySelector('.form_input-user');
+const inputClosePin = document.querySelector('.form_input-pin');
